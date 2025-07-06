98804afed219b2f93ba4eb357e3e729956e3ab90
Nikita Shestakov <nekida@gmail.com>
2025-07-06 19:16:02 +0300

    led pin update

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -53,10 +53,15 @@ void MX_GPIO_Init(void)
   HAL_GPIO_WritePin(GPIOB, LED_Pin|GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14
                           |GPIO_PIN_15, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : LED_Pin PB12 PB13 PB14
-                           PB15 */
-  GPIO_InitStruct.Pin = LED_Pin|GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14
-                          |GPIO_PIN_15;
+  /*Configure GPIO pin : LED_Pin */
+  GPIO_InitStruct.Pin = LED_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LED_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PB12 PB13 PB14 PB15 */
+  GPIO_InitStruct.Pin = GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
